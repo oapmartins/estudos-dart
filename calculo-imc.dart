1019ef393e217b82c5769455401376e5dad95fa2
oapmartins <octaviomartins10@gmail.com>
2021-07-24 17:20:05 -0300

    Refatorando e adicionando novas funcionalidades ao calculo de imc

--- calculo-imc.dart
+++ calculo-imc.dart
@@ -1,32 +1,45 @@
+import 'dart:ffi';
 import 'dart:io';
-main(){
+
+main() {
   // Fazer um calculo de IMC.
+  calculoImc();
+}
 
-  // Pegando peso.
+// Programa que calcula o imc.
+calculoImc() {
+
   print('Digite seu peso:');
-  var textPeso = stdin.readLineSync();
-  var peso = int.parse(textPeso!);
+  String textPeso = stdin.readLineSync()!;
+  int peso = int.parse(textPeso);
 
-  // Pegando altura.
   print('Digite sua altura:');
-  var textAltura = stdin.readLineSync();
-  var altura = double.parse(textAltura!);
+  String textAltura = stdin.readLineSync()!;
+  double altura = double.parse(textAltura);
+  
+  double imc = calcImcExpress(peso, altura);
+  imprimirResultado(imc);
+}
 
-  // Calculo
-  var calculoImg = peso / (altura * altura);
+// Função que recebe o peso e altura e retorna o IMC.
+double calcImcExpress(int peso, double altura) {
+  return peso / (altura * altura);
+}
 
+// Imprime o resultado com base no IMC passado.
+imprimirResultado(double imc) {
   // Prints na tela para o usuário.
-  if(calculoImg < 18.5){
+  if (imc < 18.5) {
     print('Abaixo do peso.');
-  }else if(calculoImg > 18.5 && calculoImg < 24.9){
+  } else if (imc > 18.5 && imc < 24.9) {
     print('Peso normal.');
-  }else if(calculoImg > 25 && calculoImg < 29.9){
+  } else if (imc > 25 && imc < 29.9) {
     print('Sobrepeso');
-  }else if(calculoImg > 30 && calculoImg < 34.9){
+  } else if (imc > 30 && imc < 34.9) {
     print('Obesidade grau 1');
-  }else if(calculoImg > 35 && calculoImg < 39.9){
+  } else if (imc > 35 && imc < 39.9) {
     print('Obesidade grau 2');
-  }else{
+  } else {
     print('Obeseidade grau 3');
   }
 }
